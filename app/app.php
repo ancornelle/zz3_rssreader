e02219e82b857b683c6d212284f913025545e2e3
Antoine CORNELLE <antoine.cornelle@outlook.fr>
2015-02-11 17:20:00 +0100

    First Commit

---
+++ app/app.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Antoine
+ * Date: 11/02/2015
+ * Time: 17:07
+ */
+
+    require_once __DIR__.'/../vendor/autoload.php';
+
+    $app = new Silex\Application();
+
+    // ... definitions
+
+    $app->run();
---
+++ web/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Antoine
+ * Date: 11/02/2015
+ * Time: 16:50
+ */
+
+require_once __DIR__.'/../app/app.php';
+
+echo "Hello World !";
